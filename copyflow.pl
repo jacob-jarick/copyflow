b7f85fa67eac40e336045f08af576688ef1ca623
jacob-jarick <mem.namefix@gmail.com>
2017-12-02 03:40:09 +1000

    WIP

--- copyflow.pl
+++ copyflow.pl
@@ -1,7 +1,21 @@
 #!/usr/bin/perl
 
-use 5.010;
 use strict;
 use warnings;
+use Data::Dumper::Concise;use GraphViz::Data::Grapher;
 
-print "Hello World! Copy Flow\n";
+use FindBin qw/$Bin/;
+use lib "$Bin/libs";
+use jhash;
+
+my $file = "$Bin/test.json";
+my $data = &jhash::load($file);
+my %hash = %$data;
+my $graph = GraphViz::Data::Grapher->new(%hash);
+my $tmp = $graph->as_png;
+exit;
+open( FILE, ">out.png" ) or die "$!\n";
+print FILE 1;
+close(FILE);
+
+

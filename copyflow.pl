da4132f0ea839311cb2ba067290bd72f9e77b048
jacob-jarick <mem.namefix@gmail.com>
2018-02-23 18:14:52 +0800

    WIP

--- copyflow.pl
+++ copyflow.pl
@@ -2,20 +2,32 @@
 
 use strict;
 use warnings;
-use Data::Dumper::Concise;use GraphViz::Data::Grapher;
 
 use FindBin qw/$Bin/;
 use lib "$Bin/libs";
-use jhash;
 
-my $file = "$Bin/test.json";
-my $data = &jhash::load($file);
-my %hash = %$data;
-my $graph = GraphViz::Data::Grapher->new(%hash);
-my $tmp = $graph->as_png;
+use Net::SFTP::Foreign;
+use Config::IniHash;
+
+while(@ARGV)
+{
+}
+
+
 exit;
-open( FILE, ">out.png" ) or die "$!\n";
-print FILE 1;
-close(FILE);
 
-
+sub get
+{
+}
+
+sub put
+{
+}
+
+sub fstat
+{
+}
+
+sub fhash
+{
+}
